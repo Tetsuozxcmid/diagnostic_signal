0e326cc56288ba8a3fb22140297605e747ab2f20
Smersh228 <mstislav.medvedev.2018@mail.ru>
2025-08-21 22:20:01 +0000

    ZOV

--- devicesimulator.h
+++ devicesimulator.h
@@ -1,17 +1,27 @@
 #ifndef DEVICESIMULATOR_H
 #define DEVICESIMULATOR_H
+#include "vector"
+#include "qvector.h"
+
+
 
 class DeviceSimulator
 {
 public:
-    DeviceSimulator(double amplitude, double frequency, double sampleRate);
+    DeviceSimulator(double amplitude, double frequency, double sampleRate,int N);
     double generateSample();
-
+    double generateSin();
+    double  generateCos();
+    int Nindex();
+    int Frequency();
+    double Amlitude();
+    double FrequencyD();
 private:
     double A;
     double f;
     double fd;
     double t;
+    int N;
     static const double PI;
 };
 
